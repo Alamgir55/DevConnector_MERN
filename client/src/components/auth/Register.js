9439a6b63b4e5507280b1b782f76153d472d2be1
alamgir hossain <alamgir555hossain@gmail.com>
2021-10-26 21:15:24 -0400

    redux-alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
 import React, {Fragment, useState} from 'react'
+import {Link} from 'react-router-dom'
+import { connect } from 'react-redux'
+import {setAlert} from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+const Register = ({setAlert}) => {
   const [formData, setFormData] = useState({ 
     name: '',
     email: '',
@@ -12,12 +17,12 @@ const Register = () => {
 
   const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2){
-      console.log('Passoword does not match');
+      setAlert('Passoword does not match', 'danger');
     }else {
-      console.log(formData);
+      console.log('success');
     }
   } 
 
@@ -32,8 +37,7 @@ const Register = () => {
           <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
-          >
+            Gravatar email</small>
         </div>
         <div className="form-group">
           <input
@@ -58,9 +62,14 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
      </Fragment>
 }
 
-export default Register
+Register.prototype = {
+  setAlert: PropTypes.func.isRequired,
+}
+
+export default connect(null, {setAlert})(Register)
+
