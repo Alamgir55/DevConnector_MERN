377daaa8a83bce5ee5f673f9b67a6ecf4f780561
alamgir hossain <alamgir555hossain@gmail.com>
2021-10-28 22:08:38 -0400

    ui-login-register-redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, {Fragment, useState} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
 import { connect } from 'react-redux'
 import {setAlert} from '../../actions/alert'
 import PropTypes from 'prop-types'
+import { register } from '../../actions/auth';
 
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register, isAuthenticated}) => {
   const [formData, setFormData] = useState({ 
     name: '',
     email: '',
@@ -22,16 +23,19 @@ const Register = ({setAlert}) => {
     if(password !== password2){
       setAlert('Passoword does not match', 'danger');
     }else {
-      console.log('success');
+      register({name, email, password});
     }
   } 
+  if(isAuthenticated){
+    return <Redirect to="/dashboard" />
+  }
 
   return <Fragment> 
        <h1 className="large text-primary">Sign Up</h1>
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
       <form className="form" action="create-profile.html" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
@@ -46,7 +50,6 @@ const Register = ({setAlert}) => {
             name="password"
             value={password}
             onChange={e => onChange(e)}
-            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -56,7 +59,6 @@ const Register = ({setAlert}) => {
             name="password2"
             value={password2}
             onChange={e => onChange(e)}
-            minLength="6"
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -69,7 +71,13 @@ const Register = ({setAlert}) => {
 
 Register.prototype = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 }
 
-export default connect(null, {setAlert})(Register)
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, {setAlert, register})(Register)
 
